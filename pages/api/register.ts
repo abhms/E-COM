43e0f66fd98bafef82f84a6dbdbc60bd52dccf11
Abhishek Mishra <65820065+abhms@users.noreply.github.com>
2023-06-22 15:45:19 +0530

    database connection done

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -4,7 +4,7 @@
 //   if (req.method === 'POST') {
 //     // Get the form data from the request body
 //     const { name, email, password } = req.body;
-//     
+//
 //     // Perform form validation
 //     if (!name || !email || !password) {
 //       return res.status(400).json({ error: 'All fields are required' });
@@ -55,10 +55,9 @@
 //   }
 // }
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from '../../models/User';
-import  db  from '../../models/db';
-import mongoose from "mongoose"
+import { NextApiRequest, NextApiResponse } from "next";
+import User from "../../models/User";
+import mongoose from "../../models/db";
 
 // export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 //   try {
@@ -86,39 +85,33 @@ import mongoose from "mongoose"
 //     res.status(500).json({ error: 'An internal server error occurred' });
 //   }
 // }
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        // Example: Create a new user
-        const { name, email, password } = req.body;
-        console.log(req.body, "ppppp");
-        // Establish database connection
-        // const dbConnection = await connect();
-        // const client = await clientPromise;
-        // console.log(client,"client....");
-        // const db = client.db();
-        await db();
-        // if (mongoose.connection.readyState === 1) {
-        //     console.log('Database connected');
-        //   } else {
-        //     console.log('Database not connected');
-        //   }
-        // console.log(dbConnection,"connection");
-        // Create a new user instance
-        // const newUser = new User({
-        //     name,
-        //     email,
-        //     password,
-        // });
-        // // // // console.log(dbConnection,"dbConnection");
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Example: Create a new user
+    const { name, email, password } = req.body;
+    console.log(req.body, "ppppp");
 
-        // // // //   // Save the new user to the database
-        // await newUser.save();
-
-        // Return the response
-        res.status(200).json({ message: 'User created successfully' });
-    } catch (error) {
-        // Handle errors
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An internal server error occurred' });
+    if (mongoose.connection.readyState === 1) {
+      console.log("Database connected");
+    } else {
+      console.log("Database not connected");
     }
+
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    res.status(200).json({ message: "User created successfully" });
+  } catch (error) {
+    // Handle errors
+    console.error("Error:", error);
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
 }
