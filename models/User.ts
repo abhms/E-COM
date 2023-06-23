3623d077535d5433d435727e65e449f29428eae5
abhishek22331 <abhishek.immanent@hmail.com>
2023-06-23 09:26:28 +0530

    no change

--- models/User.ts
+++ models/User.ts
@@ -1,19 +1,31 @@
-import mongoose, { Schema, Document } from 'mongoose';
+// import mongoose, { Schema, Document } from 'mongoose';
 
-// Define the User schema
-const userSchema = new Schema({
+// // Define the User schema
+// const userSchema = new Schema({
+//   name: String,
+//   email: String,
+//   password: String,
+// }, { timestamps: true });
+
+// // Define the User model
+// interface IUser extends Document {
+//   name?: string;
+//   email?: string;
+//   password?: string;
+// }
+
+// const User = mongoose.model<IUser>('User', userSchema);
+
+// export default User;
+
+import mongoose from "./db";
+
+const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-}, { timestamps: true });
+});
 
-// Define the User model
-interface IUser extends Document {
-  name?: string;
-  email?: string;
-  password?: string;
-}
-
-const User = mongoose.model<IUser>('User', userSchema);
+const User = mongoose.models.users || mongoose.model("users", UserSchema);
 
 export default User;
