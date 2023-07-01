05c83dc56d0a6f94eee921ab040a286eedecf123
abhishek mishra <abhms200@gmail.com>
2023-07-01 23:57:05 +0530

    seller form css done

--- pages/api/seller/product.ts
+++ pages/api/seller/product.ts
@@ -1,13 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import formidable from 'formidable';
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        console.log(req.body, "bodydydydyy");
-    } catch (error) {
+  try {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while parsing the form data' });
+        return;
+      }
 
-    }
+      const { description, productname, price, selectedProductType } = fields;
+      console.log(description, productname, price, selectedProductType);
+
+      // Access file data if needed
+      console.log(files);
+
+      res.status(200).json({ message: 'User created successfully' });
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'An internal server error occurred' });
+  }
 }
--- pages/seller/index.tsx
+++ pages/seller/index.tsx
@@ -3,13 +3,14 @@ import 'tailwindcss/tailwind.css';
 import { Navbar } from '../../components/Navbar';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+
 const Seller = () => {
   const router = useRouter();
   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
   const [productname, setProductname] = useState('');
   const [selectedProductType, setSelectedProductType] = useState('');
   const [price, setPrice] = useState('');
-  const [decription, setDecription] = useState('');
+  const [description, setDescription] = useState('');
   const [error, setError] = useState('');
   const productTypes = [
     'Electronics',
@@ -19,73 +20,82 @@ const Seller = () => {
     'Beauty',
     // Add more product types as needed
   ];
-  var tok:string |null
+  var tok: string | null;
   useEffect(() => {
     const localToken = localStorage.getItem('token');
-    tok=localToken
+    tok = localToken;
     if (!localToken) {
-      alert("please login to become to seller")
+      alert('Please login to become a seller');
       router.push('/auth/signin');
       return;
     }
   }, []);
-
 
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files) {
       setSelectedFiles(files);
     }
-    console.log("object", e);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (!decription || !productname || !selectedFiles || !price || !selectedProductType) {
-      setError("all fields are required*")
+    if (!description || !productname || !selectedFiles || !price || !selectedProductType) {
+      setError('All fields are required*');
       return;
     }
-    console.log(tok,"toooo");
-    const res = await axios.post('/api/seller/product', {
 
-      decription, productname, selectedFiles, price, selectedProductType, headers: {
-        Authorization: `Bearer ${tok}`
-      }
+    const formData = new FormData();
+    Array.from(selectedFiles).forEach((file) => {
+      formData.append('files', file);
     });
-    console.log(res, "resssss");
+
+    formData.append('description', description);
+    formData.append('productname', productname);
+    formData.append('price', price);
+    formData.append('selectedProductType', selectedProductType);
+
+    const res = await axios.post('/api/seller/product', formData, {
+      headers: {
+        Authorization: `Bearer ${tok}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    console.log(res, 'resssss');
   };
-  console.log(decription, productname, selectedFiles, price, selectedProductType);
 
   return (
     <>
       <Navbar />
       <br />
-      
+
       <div className="container mx-auto">
-        <form onSubmit={handleSubmit} className='sellerForm'>
-          <h1 className="text-3xl font-bold mb-4">Product decription Form</h1>
-          {error && error.length?<>
-        <h1 style={{color:"#e82617"}}>{error}</h1>
-        </>:null}
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto border rounded-md px-8 py-6 mb-4">
+          <h1 className="text-3xl font-bold mb-4">Product Description Form</h1>
+          {error && error.length ? <h1 className="text-red-500">{error}</h1> : null}
+
           <div className="mb-4">
             <label htmlFor="name" className="block text-lg font-medium mb-2">
-              Product Name:
+              Product Name
             </label>
             <input
               type="text"
               id="name"
-              className="px-4 py-2 border border-gray-300 rounded"
+              placeholder='Product Name'
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
               value={productname}
               onChange={(e) => setProductname(e.target.value)}
             />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="productType" className="block mb-2 text-sm font-medium text-gray-900">
+            <label htmlFor="productType" className="block text-lg font-medium mb-2">
               Product Type
             </label>
             <select
               id="productType"
-              className="sellerDrop block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
               value={selectedProductType}
               onChange={(e) => setSelectedProductType(e.target.value)}
             >
@@ -97,45 +107,54 @@ const Seller = () => {
               ))}
             </select>
           </div>
+
           <div className="mb-4">
             <label htmlFor="files" className="block text-lg font-medium mb-2">
               Files:
             </label>
-            <input
-              type="file"
-              id="files"
-              multiple
-              onChange={handleFileChange}
-            />
-            {selectedFiles && selectedFiles?.length > 0 && (
-              <span className="ml-2">{selectedFiles.length} files selected</span>
-            )}
+            <div className="flex items-center">
+              <label
+                htmlFor="fileInput"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+              >
+                Select Files
+              </label>
+              <span className="ml-4">{selectedFiles?.length ?? 0} files selected</span>
+              <input
+                type="file"
+                id="fileInput"
+                multiple
+                onChange={handleFileChange}
+                className="hidden"
+              />
+            </div>
           </div>
 
           <div className="mb-4">
-            <label htmlFor="name" className="block text-lg font-medium mb-2">
-              Product Price(in crypto):
+            <label htmlFor="price" className="block text-lg font-medium mb-2">
+              Product Price (in crypto):
             </label>
             <input
               type="text"
-              id="name"
-              className="px-4 py-2 border border-gray-300 rounded"
+              id="price"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
               value={price}
-              placeholder='Price'
+              placeholder="Price"
               onChange={(e) => setPrice(e.target.value)}
             />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Product decription
+            <label htmlFor="message" className="block text-lg font-medium mb-2">
+              Product Description
             </label>
             <textarea
               id="message"
               rows={4}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Please provide product decription"
-              value={decription}
-              onChange={(e) => setDecription(e.target.value)}
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+              placeholder="Please provide product description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             ></textarea>
           </div>
 
