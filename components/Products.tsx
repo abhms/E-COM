d289194b0fa7936e16dc032ac855d61b238ba79c
abhishek mishra <abhms200@gmail.com>
2023-07-19 22:53:27 +0530

    cart css change with footer

--- components/Products.tsx
+++ components/Products.tsx
@@ -13,24 +13,33 @@ interface ProductListProps {
 }
 
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
-    //@ts-ignore
-    console.log(products,"prop");
+  //@ts-ignore
+  console.log(products, "prop");
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {products.map((product) => (
+      {products && products.map((product) => (
         <div key={product.id} className="bg-white shadow-md rounded-lg p-4">
+          <div className='cartImg'>
             {/**@ts-ignore */}
-          <img src={product.fileUrl}
-            alt={product.name}
-            className="w-full h-32 object-cover mb-4"
-          />
+            <img src={product.fileUrl}
+              alt={product.name}
+              className="cartImg"
+            />
+          </div>
           {/**@ts-ignore */}
           <h2 className="text-xl font-bold mb-2">{product.productname}</h2>
-          <p className="text-gray-600 mb-2">${product.price}</p>
+          <p className="text-gray-600 mb-2">${product.price} BNB</p>
           <p className="text-gray-800">{product.description}</p>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Add to Cart
+        </button>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mx-7 cartbtn">
+          Buy
+        </button>
         </div>
       ))}
     </div>
+
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Navbar } from "../components/Navbar";
 import axios from 'axios';
 import ProductList from '../components/Products';
+import Footer from '../components/Footer';
 const Home: React.FC = () => {
   const [products, setProducts] = useState([]);
 
@@ -22,10 +23,13 @@ const Home: React.FC = () => {
    console.log(products.product,"propooo");
   return (
     <>
+    <div>
       <Navbar />
       <div className="container mx-auto p-4">
       {/*@ts-ignore*/}
         <ProductList products={products.product} />
+      </div>
+      <Footer/>
       </div>
     </>
   );
